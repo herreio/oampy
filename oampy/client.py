7c7fb978401fb65fb71d3a4504b74b9ac1de5742
Donatus Herre <donatus.herre@slub-dresden.de>
2022-03-09 12:06:45 +0100

    Update methods and attributes of OAM client

--- oampy/client.py
+++ oampy/client.py
@@ -4,11 +4,22 @@ from . import utils
 class OpenAccessMonitorAPI:
 
     def __init__(self):
-        self.BASE = "https://open-access-monitor.de/api/Data/public"
+        self.BASE = "https://open-access-monitor.de/api"
+        self.DATA = "{0}/Data".format(self.BASE)
+        self.PUBLIC = "{0}/public".format(self.DATA)
+
+    def info(self):
+        return utils.json_request(self.BASE)
+
+    def _databases(self):
+        return utils.json_request(self.DATA)
+
+    def collections(self):
+        return utils.json_request(self.PUBLIC)
 
     def get(self, find, filter=None, limit=10, headers={}):
         query = self.query(find, filter=filter, limit=limit)
-        url = "{0}?query={1}".format(self.BASE, query)
+        url = "{0}?query={1}".format(self.PUBLIC, query)
         response = utils.json_request(url, headers=headers)
         if response and response["ok"] == 1:
             return response["cursor"]
