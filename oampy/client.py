f498ed30cea6ede7fd34914ca554415301bb9df0
Donatus Herre <donatus.herre@slub-dresden.de>
2022-03-11 21:54:25 +0100

    Update OAM API client methods

--- oampy/client.py
+++ oampy/client.py
@@ -22,11 +22,22 @@ class OpenAccessMonitorAPI:
         url = "{0}?query={1}".format(self.PUBLIC, query)
         response = utils.json_request(url, headers=headers)
         if response and response["ok"] == 1:
-            return response["cursor"]
+            return self.batch(response)
+
+    @staticmethod
+    def batch(response):
+        if "cursor" in response:
+            if "firstBatch" in response["cursor"]:
+                return response["cursor"]["firstBatch"]
 
     @staticmethod
     def query(find, filter=None, limit=10):
         q = {"find": find, "limit": limit}
         if filter is not None:
             q["filter"] = filter
+        return utils.json_str(q)
+
+    @staticmethod
+    def cursor_query(id, collection, batch_size=10):
+        q = {"getMore": id, "collection": collection, "batch_size": batch_size}
         return utils.json_str(q)
