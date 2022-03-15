b4728fadf2c70aa7ce99a88cba748f8af7dd4abd
Donatus Herre <donatus.herre@slub-dresden.de>
2022-03-15 19:54:03 +0100

    Add long_description to setup

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ setup(
     author="Donatus Herre",
     author_email="donatus.herre@slub-dresden.de",
     description="Open Access Monitor API Client",
+    long_description=open("README.rst").read(),
+    long_description_content_type="text/x-rst",
     license="GPLv3",
     url="https://github.com/herreio/oampy",
     packages=["oampy"],
