936b7d1d557603192adab5fdfafc21ced5a3c16f
Donatus Herre <donatus.herre@slub-dresden.de>
2022-03-20 12:40:34 +0100

    Bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="oampy",
-    version="0.1.7",
+    version="0.1.8",
     author="Donatus Herre",
     author_email="donatus.herre@slub-dresden.de",
     description="Open Access Monitor API Client",
